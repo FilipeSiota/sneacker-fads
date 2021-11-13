84855bc23ebed5ae94609c4895f6d3a00ec5a24a
Filipe Mallmann Siota <filipesiota@gmail.com>
2021-11-13 14:21:08 -0300

    Aula 1 - Histórico e Introdução à JavaScript

---
+++ README.md
@@ -0,0 +1,17 @@
+# CÓDIGOS IMPLEMENTANDO JAVASCRIPT
+
+- [x] Alterar ao menos um atributo de um elemento HTML;
+> Verificar arquivos `script.js` e `index.html`
+
+- [x] Alterar o estilo de ao menos um elemento HTML;
+> Verificar arquivos `script.js` e `index.html`
+
+- [x] Alterar ao menos um conteúdo HTML;
+> Verificar arquivos `script.js` e `index.html`
+
+- [x] Ao menos uma forma de inserir o JavaScript;
+> A inserção do código foi feito tanto por **arquivo externo `script.js`**, como através da **tag `<script>`** no corpo do `index.html`
+
+- [x] Ao menos duas forma de output (saída) resultante de código de JavaScript;
+> `.innerHTML` ==> Verificar div (id="bottom") do arquivo `index.html`<br>
+> `window.alert()` ==> Verificar arquivo `script.js`
--- js/script.js
+++ js/script.js
@@ -9,6 +9,24 @@ menuIcon.addEventListener('click', () => {
     body.classList.toggle('disable-scroll');
 })
 
+// TROCA O BACKGROUND-IMAGE DA DIV IMAGE-EFFECT ==> ALTERAÇÃO DE ESTILO DE UM ELEMENTO HTML
+
+const imageEffect = document.getElementById('slide');
+var i = 0;
+
+function changeBackground() {
+    if (i % 2 == 0)
+    {
+        imageEffect.style.backgroundImage = "url(../img/lil_nas_x_nike_1280px.jpg)";
+    }
+    else
+    {
+        imageEffect.style.backgroundImage = "url(../img/prateleira-tenis-1280px.jpg)";
+    }
+
+    i++;
+}
+
 // TROCA COR DO ICONE DE CORAÇÃO
 
 const heartIcon = document.querySelectorAll('.icon-heart');
@@ -17,4 +35,43 @@ heartIcon.forEach(heart => {
     heart.addEventListener('click', () => {
         heart.classList.toggle('turn-red');
     })
+})
+
+// DÁ UM ALERTA DE QUE A PESSOA ADICIONOU O PRODUTO AO CARRINHO ==> UMA FORMA DE OUTPUT RESULTANTE DE CÓDIGO JAVASCRIPT >> window.alert()
+
+const cartIcon = document.querySelectorAll('.fi-sr-shopping-cart-add');
+
+cartIcon.forEach(cart => {
+    cart.addEventListener('click', () => {
+        window.alert("Produto adicionado ao carrinho com sucesso!");
+
+        cart.style.color = "var(--theme3)";
+    })
+})
+
+// MODIFICA ATRIBUTO HREF DE UM LINK QUE SERÁ USADO PARA PESQUISA E ALTERA O CONTEÚDO DO PARÁGRAFO PARA ESTAR DE ACORDO COM A PESQUISA
+
+const divSearch = document.getElementById('searching');
+const divContent = document.getElementById('content-plus-link');
+const input = document.getElementById('search');
+const btnSearch = document.getElementById('getSearch');
+const extraContent = document.getElementById('extra');
+const link = document.getElementById('link-creator');
+
+var content;
+
+btnSearch.addEventListener('click', () => {
+    content = input.value;
+
+    if (content != "") {
+
+        divSearch.style.display = "flex";
+        divContent.style.display = "block";
+
+        extraContent.innerHTML = "Dê uma olhadinha no site da nossa patrocinadora <strong>Netshoes</strong>, talvez você encontre o produto:<br>&quot;" + content + "&quot;";
+
+        link.href = "https://www.netshoes.com.br/busca?q=" + content;
+
+        link.innerText = "Clique aqui";
+    }
 })
